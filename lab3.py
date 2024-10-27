aac570e574b801e7fceaedb6f6eb1fcce38b476d
Blyatman0692 <jzheng2@seattleu.edu>
2024-10-27 16:54:34 -0700

    functionalities to detect new quotes and replace existing quotes with newer quotes

--- lab3.py
+++ lab3.py
@@ -1,10 +1,7 @@
-from shlex import quote
-
 import bellman_ford
 import fxp_bytes_subscriber
 import socket
 import threading
-import queue
 
 REQUEST_ADDRESS = ('localhost', 10101)
 
@@ -23,7 +20,7 @@ class UDPSubscriber:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind(self.addr)
         self.running = False
-        self.quotes_list = []
+        self.valid_quotes = {}
 
     def connect(self):
         self.socket.sendto(fxp_bytes_subscriber.marshal_sub_request
@@ -32,9 +29,34 @@ class UDPSubscriber:
     def listen(self):
         while self.running:
             incoming_bytes = self.socket.recv(4096)
-            quotes = fxp_bytes_subscriber.unmarshal_quotes(incoming_bytes)
-            print(quotes[0])
-            self.quotes_list.append(quotes)
+            unprocessed_quotes = fxp_bytes_subscriber.unmarshal_quotes(incoming_bytes)
+            self.process_quotes(unprocessed_quotes)
+
+    def process_quotes(self, quotes):
+        for quote in quotes:
+            currency_pair = quote['cross']
+            if currency_pair not in self.valid_quotes:
+                self.valid_quotes[currency_pair] = {
+                    'price': quote['price'],
+                    'time': quote['time']
+                }
+                print(f"New quote detected, adding:{currency_pair}: {self.valid_quotes[currency_pair]}")
+
+            if quote['time'] > self.valid_quotes[currency_pair]['time']:
+                self.valid_quotes[currency_pair] = {
+                    'price': quote['price'],
+                    'time': quote['time']
+                }
+                print(f"Existing quote detected, replacing with new quote:{currency_pair}: {self.valid_quotes[currency_pair]}")
+
+            ## no need for out-of-order detection since new quotes are only added if it has bigger timestamp
+            # if (currency_pair in self.valid_quotes) and (
+            #         quote['time'] < self.valid_quotes[currency_pair]['time']):
+            #     # Detected an out-of-order quote, ignore it
+            #     print(
+            #         f"****Detected out-of-order quote for {currency_pair} with timestamp {quote['time']}")
+            #     continue
+
 
     def start_listener_thread(self):
         listener_thread = threading.Thread(target=self.listen())
@@ -45,7 +67,6 @@ class UDPSubscriber:
         self.running = True
         self.connect()
         self.start_listener_thread()
-
 
 
 if __name__ == '__main__':
