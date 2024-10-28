dd5edb544cb41c20d53df55fa08ec8f5d73998cb
Blyatman0692 <jzheng2@seattleu.edu>
2024-10-27 21:42:01 -0700

    added lock to QuotesManager

--- lab3.py
+++ lab3.py
@@ -1,6 +1,4 @@
 from datetime import datetime, timedelta
-
-
 import bellman_ford
 import fxp_bytes_subscriber
 import socket
@@ -11,50 +9,54 @@ REQUEST_ADDRESS = ('localhost', 10101)
 class QuotesManager:
     def __init__(self):
         self.latest_valid_quotes = {}
+        self.lock = threading.Lock()
 
     def process_quotes(self, quotes):
         for quote in quotes:
             currency_pair = quote['cross']
-            if currency_pair not in self.latest_valid_quotes:
-                self.latest_valid_quotes[currency_pair] = {
-                    'price': quote['price'],
-                    'time': quote['time']
-                }
-                print(
-                    f"New quote detected, adding:{currency_pair}: "
-                    f"{self.latest_valid_quotes[currency_pair]}")
+            with self.lock:
+                if currency_pair not in self.latest_valid_quotes:
+                    self.latest_valid_quotes[currency_pair] = {
+                        'price': quote['price'],
+                        'time': quote['time']
+                    }
+                    print(
+                        f"New quote detected, adding:{currency_pair}: "
+                        f"{self.latest_valid_quotes[currency_pair]}")
 
-            if quote['time'] > self.latest_valid_quotes[currency_pair]['time']:
-                self.latest_valid_quotes[currency_pair] = {
-                    'price': quote['price'],
-                    'time': quote['time']
-                }
-                print(
-                    f"Existing quote detected, replacing with new quote:{currency_pair}: "
-                    f"{self.latest_valid_quotes[currency_pair]}")
+                if quote['time'] > self.latest_valid_quotes[currency_pair]['time']:
+                    self.latest_valid_quotes[currency_pair] = {
+                        'price': quote['price'],
+                        'time': quote['time']
+                    }
+                    print(
+                        f"Existing quote detected, replacing with new quote:{currency_pair}: "
+                        f"{self.latest_valid_quotes[currency_pair]}")
 
-        self.remove_expired_quotes()
+            self.remove_expired_quotes()
 
     def remove_expired_quotes(self):
-        curr_time = datetime.utcnow()
-        # todo: fix expiry time limit
-        expiry_limit = curr_time - timedelta(seconds=3)
+        #with self.lock:
+            curr_time = datetime.utcnow()
+            # todo: fix expiry time limit
+            expiry_limit = curr_time - timedelta(seconds=3)
 
-        expiry_keys = []
+            expiry_keys = []
 
-        for quote_key, quote_info in self.latest_valid_quotes.items():
-            if quote_info['time'] < expiry_limit:
-                expiry_keys.append(quote_key)
-
-        for key in expiry_keys:
-            print(f"Removing expired quote for {key}")
-            del self.latest_valid_quotes[key]
+            for quote_key, quote_info in self.latest_valid_quotes.items():
+                if quote_info['time'] < expiry_limit:
+                    expiry_keys.append(quote_key)
+            with self.lock:
+                for key in expiry_keys:
+                    print(f"Removing expired quote for {key}")
+                    del self.latest_valid_quotes[key]
 
     def get_valid_quotes(self):
-        return self.latest_valid_quotes
+        with self.lock:
+            return self.latest_valid_quotes.copy()
 
 class UDPSubscriber:
-    def __init__(self, server_addr, listen_ip, listen_port):
+    def __init__(self, server_addr, listen_ip, listen_port, quotes_manager):
         self.server_addr = server_addr
         self.listen_ip = listen_ip
         self.listen_port = listen_port
@@ -62,20 +64,35 @@ class UDPSubscriber:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind(self.addr)
         self.running = False
-        self.quotes_manager = QuotesManager()
+        self.quotes_manager = quotes_manager
 
     def connect(self):
         self.socket.sendto(fxp_bytes_subscriber.marshal_sub_request
                            (self.listen_ip, self.listen_port), self.server_addr)
 
+    # def listen(self):
+    #     while self.running:
+    #         incoming_bytes = self.socket.recv(4096)
+    #         unprocessed_quotes = fxp_bytes_subscriber.unmarshal_quotes(incoming_bytes)
+    #         self.quotes_manager.process_quotes(unprocessed_quotes)
+
     def listen(self):
         while self.running:
-            incoming_bytes = self.socket.recv(4096)
-            unprocessed_quotes = fxp_bytes_subscriber.unmarshal_quotes(incoming_bytes)
-            self.quotes_manager.process_quotes(unprocessed_quotes)
+            try:
+                incoming_bytes = self.socket.recv(4096)
+                if incoming_bytes:
+                    unprocessed_quotes = fxp_bytes_subscriber.unmarshal_quotes(
+                        incoming_bytes)
+                    self.quotes_manager.process_quotes(unprocessed_quotes)
+            except socket.error as e:
+                print(f"Socket error: {e}")
+                self.stop_running()
+            except Exception as e:
+                print(f"Error: {e}")
+                self.stop_running()
 
     def start_listener_thread(self):
-        listener_thread = threading.Thread(target=self.listen())
+        listener_thread = threading.Thread(target=self.listen)
         listener_thread.daemon = True
         listener_thread.start()
 
@@ -84,10 +101,21 @@ class UDPSubscriber:
         self.connect()
         self.start_listener_thread()
 
+    def stop_running(self):
+        self.running = False
+        self.socket.close()
+        print("Subscriber stopped. Socket closed.")
+
 
 if __name__ == '__main__':
-    sub = UDPSubscriber(REQUEST_ADDRESS, '127.0.0.1', 50000)
+    quotes_manager = QuotesManager()
+    sub = UDPSubscriber(REQUEST_ADDRESS, '127.0.0.1', 50000, quotes_manager)
     sub.run()
+    while True:
+        pass
+
+
+
 
 
 
